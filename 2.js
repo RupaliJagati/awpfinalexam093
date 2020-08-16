54ee3edc4fd65b9f5a191d80f48e4104cb9b6c19
RupaliJagati <63409986+RupaliJagati@users.noreply.github.com>
2020-08-16 16:24:11 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,8 @@
+function submit()
+{
+         var username = document.querySelector(".username").value;
+            document.querySelector(".username1").innerHTML = username;
+            var password = document.querySelector(".password").value;
+            document.querySelector(".password1").innerHTML = password;
+            
+}
